5aff9c3f30f8da7e27f5a3504c0ad262421da86e
Adeniyi Janet <adeniyijanet61@gmail.com>
2023-08-05 05:49:09 +0100

    update to deploy to product

--- api/index.js
+++ api/index.js
@@ -1,40 +1,48 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const dotenv = require("dotenv")
-dotenv.config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const userRouter = require("./routes/UserRoute")
-const tweetRouter = require("./routes/TweetRoute")
-const bodyParser = require('body-parser');
+const userRouter = require("./routes/UserRoute");
+const tweetRouter = require("./routes/TweetRoute");
+const bodyParser = require("body-parser");
 
-app.use(cors({ credentials: true, origin:[ "http://localhost:3000", "https://micro-blog-app.onrender.com"] }))
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "https://micro-blog-app.onrender.com"],
+  })
+);
 
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // Connect server to database
-mongoose.connect(process.env.MONGO_URI)
- .then(() => {
-  console.log("Database is connected successfully");
- })
- .catch((err) => {
-  console.log(err);
- })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Configure body-parser middleware
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // middlewae for post http model
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api", userRouter)
-app.use("/api", tweetRouter)
+app.use("/api", userRouter);
+app.use("/api", tweetRouter);
 
-const PORT = 7000
-app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
-})
+if (process.env.API_PORT) {
+  app.listen(process.env.API_PORT, () => {
+    console.log(`Server running on port ${process.env.API_PORT}`);
+  });
+}
+module.exports = app;
--- client/micro-blog-app/src/Pages/AllTweets.js
+++ client/micro-blog-app/src/Pages/AllTweets.js
@@ -1,35 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom'
-import { formatDistanceToNowStrict } from "date-fns"
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { formatDistanceToNowStrict } from "date-fns";
 import { FaThumbsDown } from "react-icons/fa6";
 import { FaThumbsUp } from "react-icons/fa6";
 import { VscAdd } from "react-icons/vsc";
-import { UserContext } from '../UserContext';
-
-
+import { UserContext } from "../UserContext";
 
 // const { _id } = useParams
 // console.log(_id);
 
 const AllTweets = (id) => {
-  const [data, setData] = useState([])
-  const currentID = JSON.parse(localStorage.getItem('currentUser'))
+  const [data, setData] = useState([]);
+  const currentID = JSON.parse(localStorage.getItem("currentUser"));
   console.log(currentID);
   // const { setUserInfo, userInfo } = useContext(UserContext);
-
-  const [tweets, setTweets] = useState([])
+  const url = process.env.REACT_APP_URL;
+  const [tweets, setTweets] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:7000/api/tweet/home').then(response => {
-      response.json().then(posts => {
+    fetch(`${url}/tweet/home`).then((response) => {
+      response.json().then((posts) => {
         console.log(posts);
         setTweets(posts);
-      })
-    })
-  }, [])
+      });
+    });
+  }, []);
 
   const likeTweet = (idendti, currentID) => {
-
-    fetch('http://localhost:7000/api/tweet/like', {
+    fetch(`${url}/tweet/like`, {
       method: "put",
       headers: {
         "Content-Type": "application/json",
@@ -37,25 +34,27 @@ const AllTweets = (id) => {
       body: JSON.stringify({
         id: currentID,
         tweetId: idendti,
-      })
-    }).then(res => res.json())
-      .then(result => {
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
         // console.log(result);
-        const newData = data.map(item => {
+        const newData = data.map((item) => {
           if (item._id == result._id) {
-            return result
+            return result;
           } else {
-            return item
+            return item;
           }
-        })
-        setData(newData)
-      }).catch(err => {
-        console.log(err);
+        });
+        setData(newData);
       })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const unlikeTweet = (identi, currentID) => {
-    fetch('http://localhost:7000/api/tweet/unlike', {
+    fetch(`${url}tweet/unlike`, {
       method: "put",
       headers: {
         "Content-Type": "application/json",
@@ -63,61 +62,79 @@ const AllTweets = (id) => {
       body: JSON.stringify({
         id: currentID,
         tweetId: identi,
-      })
-    }).then(res => res.json())
-      .then(result => {
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
         // console.log(result);
-        const newData = data.map(item => {
+        const newData = data.map((item) => {
           if (item._id == result._id) {
-            return result
+            return result;
           } else {
-            return item
+            return item;
           }
-        })
-        setData(newData)
-      }).catch(err => {
-        console.log(err);
+        });
+        setData(newData);
       })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
-      {tweets.length > 0 && tweets.map(tweet => {
-        return (
-          <div className='tweet-layout' key={tweet._id}>
-            <div className='image-contain'>
-              <img src={tweet.postedBy.imageURL} className='tweet-img' />
-            </div>
-
-            <div className='tweet-text-img'>
-              <div style={{ display: "flex" }}>
-                <p className='tweet-username'>{tweet.postedBy.username}</p>
-                <time className='tweet-time'>-{formatDistanceToNowStrict(new Date(tweet.createdAt))}</time>
-
+      {tweets.length > 0 &&
+        tweets.map((tweet) => {
+          return (
+            <div className="tweet-layout" key={tweet._id}>
+              <div className="image-contain">
+                <img src={tweet.postedBy.imageURL} className="tweet-img" />
               </div>
 
-              <div>
-              <p className='tweet-tweet'>{tweet.tweet}</p>
-              <img src={'http://localhost:7000/' + tweet.photo} alt="" className='tweet-image' />
-              <div>
-                {tweet.likes.includes(currentID._id)
-                  ? <button onClick={() => { unlikeTweet(tweet._id, currentID._id) }} className='down'><FaThumbsDown /></button>
-                  :
-                    <button onClick={() => { likeTweet(tweet._id, currentID._id) }} className='up'><FaThumbsUp /></button>
-                }
-                <p>{tweet.likes.length} Likes</p>
+              <div className="tweet-text-img">
+                <div style={{ display: "flex" }}>
+                  <p className="tweet-username">{tweet.postedBy.username}</p>
+                  <time className="tweet-time">
+                    -{formatDistanceToNowStrict(new Date(tweet.createdAt))}
+                  </time>
+                </div>
+
+                <div>
+                  <p className="tweet-tweet">{tweet.tweet}</p>
+                  <img
+                    src={"http://localhost:7000/" + tweet.photo}
+                    alt=""
+                    className="tweet-image"
+                  />
+                  <div>
+                    {tweet.likes.includes(currentID._id) ? (
+                      <button
+                        onClick={() => {
+                          unlikeTweet(tweet._id, currentID._id);
+                        }}
+                        className="down"
+                      >
+                        <FaThumbsDown />
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => {
+                          likeTweet(tweet._id, currentID._id);
+                        }}
+                        className="up"
+                      >
+                        <FaThumbsUp />
+                      </button>
+                    )}
+                    <p>{tweet.likes.length} Likes</p>
+                  </div>
+                </div>
               </div>
             </div>
-
-            </div>
-          </div>
-        )
-      })}
+          );
+        })}
     </>
-  )
-}
-
-
-
+  );
+};
 
 export default AllTweets;
